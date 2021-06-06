25e95c35cc2e9fe05fc90292b770659b551323cc
Josh Morrison <josh.morrison1999@gmail.com>
2021-06-05 21:16:05 -0400

    Initial Commit

---
+++ RunnersHigh/RunnersHighApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnersHighApp.swift
+//  RunnersHigh
+//
+//  Created by Josh Morrison on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct RunnersHighApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
